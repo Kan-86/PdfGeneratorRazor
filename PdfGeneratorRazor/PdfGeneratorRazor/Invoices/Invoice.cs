de953f5f51a5ccbfa491cab9ca870d46bb43809b
Kristófer Andersen <andersen19@gmail.com>
2025-01-20 14:03:00 +0100

    PDFGen

---
+++ PdfGeneratorRazor/PdfGeneratorRazor/Invoices/Invoice.cs
@@ -0,0 +1,32 @@
+﻿namespace PDFGenConsole.Invoices;
+public class Invoice
+{
+    public string Number { get; set; }
+    public DateOnly IssueDate { get; set; }
+    public DateOnly DueDate { get; set; }
+    public Address SellerAddress { get; set; }
+    public Address CustomerAddress { get; set; }
+    public List<OrderItem> Items { get; set; }
+    public string Comments { get; set; }
+}
+
+public class OrderItem
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
+
+public class Address
+{
+    public string CompanyName { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Website { get; set; }
+
+}
---
+++ PdfGeneratorRazor/PdfGeneratorRazor/Invoices/InvoiceGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Bogus;
+
+namespace PDFGenConsole.Invoices;
+public class InvoiceGenerator
+{
+    public static Invoice Generate()
+    {
+        var faker = new Faker();
+
+        var invoice = new Invoice
+        {
+
+            Number = faker.Random.Number(1000, 9999).ToString(),
+            IssueDate = faker.Date.SoonDateOnly(0),
+            DueDate = faker.Date.SoonDateOnly(0).AddMonths(1),
+            CustomerAddress = new Address
+            {
+                CompanyName = "ABC Inc.",
+                Street = "123 Main St.",
+                City = "Springfield",
+                PostalCode = "12345",
+                Country = "USA",
+                Phone = faker.Phone.PhoneNumber(),
+                Email = faker.Internet.Email(),
+            },
+            SellerAddress = new Address
+            {
+                CompanyName = "ABC Inc.",
+                Street = "123 Main St.",
+                City = "Springfield",
+                PostalCode = "12345",
+                Country = "USA",
+                Phone = faker.Phone.PhoneNumber(),
+                Email = faker.Internet.Email(),
+            },
+            Items = new List<OrderItem>
+            {
+                new OrderItem
+                {
+                    Name = "Widget",
+                    Price = 9.99m,
+                    Quantity = 10
+                },
+                new OrderItem
+                {
+                    Name = "Gadget",
+                    Price = 19.99m,
+                    Quantity = 5
+                }
+            },
+        };
+        return invoice;
+    }
+}
